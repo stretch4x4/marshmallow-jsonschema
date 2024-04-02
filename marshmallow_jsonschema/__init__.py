92359c3a364d84eef74985d92fc8b59aed00b4c0
Stephen J. Fuhry <steve@tpastream.com>
2024-04-02 15:03:57 +0000

    run python 3.12 in the build, formatting tweaks

--- marshmallow_jsonschema/__init__.py
+++ marshmallow_jsonschema/__init__.py
@@ -1,9 +1,9 @@
-from pkg_resources import get_distribution
+from importlib.metadata import version
 
-__version__ = get_distribution("marshmallow-jsonschema").version
+__version__ = version("marshmallow-jsonschema")
 __license__ = "MIT"
 
 from .base import JSONSchema
 from .exceptions import UnsupportedValueError
 
-__all__ = ("JSONSchema", "UnsupportedValueError")
+__all__ = ("JSONSchema", "UnsupportedValueError", "__version__", "__license__")
